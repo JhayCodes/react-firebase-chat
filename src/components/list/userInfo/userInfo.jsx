96f0ada30731d974275f87252fa2c6d2e6965cfd
JhayCodes <jhayycodes@gmail.com>
2024-07-31 22:18:12 +0000

    created chatlist and user info

---
+++ src/components/list/userInfo/userInfo.jsx
@@ -0,0 +1,19 @@
+import "./userInfo.css"
+
+const UserInfo = () => {
+    return (
+        <div className="userInfo">
+            <div className="user">
+                <img src="" alt="" />
+                <h2></h2>
+            </div>
+            <div className="icons">
+                <img src="" alt="" />
+                <img src="" alt="" />
+                <img src="" alt="" />
+            </div>
+        </div>
+    )
+}
+
+export default UserInfo
