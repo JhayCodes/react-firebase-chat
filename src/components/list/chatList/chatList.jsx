46633626c3a129f42d473be6969859833788707f
JhayCodes <jhayycodes@gmail.com>
2024-08-07 22:13:23 +0000

    added the 'adduser' modal

--- src/components/list/chatList/chatList.jsx
+++ src/components/list/chatList/chatList.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react"
 import "./chatList.css"
+import AddUser from "./addUser/addUser";
 
 const ChatList = () => {
     const [addMode, setAddMode] = useState(false);
@@ -31,8 +32,9 @@ const ChatList = () => {
                 </div>
 
             </div>
+            {addMode && <AddUser />}
         </div>
-    )
-}
+    );
+};
 
-export default ChatList
+export default ChatList;
