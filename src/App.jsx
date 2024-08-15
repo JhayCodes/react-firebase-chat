2fdf0863c43457aa7f8ee886db228c48241ac35f
JhayCodes <jhayycodes@gmail.com>
2024-08-15 22:09:36 +0000

    Keep track of users signed In

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,22 @@ import List from "./components/list/list"
 import Detail from "./components/detail/detail"
 import Login from "./components/login/Login"
 import Notification from "./components/notification/Notification"
+import { onAuthStateChanged } from "firebase/auth"
+import { userAuth } from "./components/lib/firebase"
 
 const App = () => {
 
   const user = false;
+
+  useEffect(() => {
+    const unSub = onAuthStateChanged(userAuth,(user) => {
+      console.log(user);
+    });
+
+    return () => {
+      unSub();
+    }
+  }, [])
 
   return (
     <div className='container'>
